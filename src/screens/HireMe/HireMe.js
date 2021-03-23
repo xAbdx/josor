c21a7b528670902d8543ab1dbde1b29ea289cce9
xAbdx <67425264+xAbdx@users.noreply.github.com>
2021-03-24 00:25:10 +0200

    fixing some inputs and buttons in HireMe.js and newJob.js and their style as well

--- src/screens/HireMe/HireMe.js
+++ src/screens/HireMe/HireMe.js
@@ -4,7 +4,7 @@ import Header from '../../components/header/header';
 import Footer from "../../components/footer/footer";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-// import { MdAttachFile } from "react-icons/md"; 
+import { MdAttachFile } from "react-icons/md"; 
 // use it for the id and type of disability
 
 const HireMe = () => {
@@ -22,30 +22,43 @@ const HireMe = () => {
                         <TextField className={classes.inputField1} id="outlined-basic" label="Full Name AF" variant="outlined" disabled />
                         <TextField className={classes.inputField1} id="outlined-basic" label="Phone AF" variant="outlined" disabled />
                         <TextField className={classes.inputField1} required id="outlined-basic" label="Location" variant="outlined" />
-
-                        {/* <label htmlFor="contained-button-file">
-                            <Button variant="contained" color="primary" component="span">
-                                kind of disability
-                            </Button>
-                        </label>
-                        <input
-                            accept="image/*"
-                            className={classes.input}
-                            id="contained-button-file"
-                            multiple
-                            type="file"
-                        /> */}
-
                         <TextField className={classes.inputField1} required id="outlined-basic" label="kind of disability" variant="outlined" />
-                        <TextField className={classes.inputField1} required id="outlined-basic" label="Your ID" variant="outlined" />
-                        <TextField className={classes.inputField1} required id="outlined-basic" label="Your supporting documents" variant="outlined" />
+                        
+                        <div className={classes.inputImage}>
+                            <label htmlFor="contained-button-file">
+                                <Button variant="contained" color="primary" component="span" endIcon={<MdAttachFile />}>
+                                    Your ID
+                                </Button>
+                                <input
+                                accept="image/*"
+                                className={classes.input}
+                                id="contained-button-file"
+                                multiple
+                                type="file"
+                            />
+                            </label>
+                            <label htmlFor="contained-button-file">
+                                <Button variant="contained" color="primary" component="span" endIcon={<MdAttachFile />}>
+                                    Your supporting documents
+                                </Button>
+                                <input
+                                accept="image/*"
+                                className={classes.input}
+                                id="contained-button-file"
+                                multiple
+                                type="file"
+                            />
+                            </label>
+                        </div>
                     </div>
                     <div className={classes.item}>
                         <Button
                             className={classes.btn}
                             orientation="vertical"
                             aria-label="vertical contained primary button group"
-                            variant="contained">Submit</Button>
+                            variant="contained">
+                            Submit
+                        </Button>
                     </div>
                 </div>
             </div>
